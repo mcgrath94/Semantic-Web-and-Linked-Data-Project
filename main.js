4266a83c20808b2d9d8f9a24303598787be0c762
mcgrath94 <mcgrath94@hotmail.com>
2015-11-18 00:20:45 +0000

    updated main.js

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ var fs = require('fs');
 
 var travMeans = JSON.parse(fs.readFileSync('travelMeans.json','utf8'))
 
-var db = new sqlite3.Database(':memory:');
+var db = new sqlite3.Database('testdb');
 
 //first table "travMeans"
 db.serialize(function() {
-  db.run('CREATE TABLE travMeans (areacode TEXT, areatype TEXT, area TEXT, traveltype TEXT, numpeople REAL)');
+  db.run('CREATE TABLE IF NOT EXISTS travMeans (areacode TEXT, areatype TEXT, area TEXT, traveltype TEXT, numpeople REAL)');
   var stmt = db.prepare('INSERT INTO travMeans (areacode, areatype, area, traveltype, numpeople) VALUES (?,?,?,?,?)');
   for (var i = 0; i < travMeans.length; i++) {
       stmt.run(travMeans[i].AreaCode
@@ -30,7 +30,7 @@ db.serialize(function() {
 var houseCars = JSON.parse(fs.readFileSync('householdCars.json','utf8'))
     
  db.serialize(function() {
-  db.run('CREATE TABLE houseCars (areacode1 TEXT, areatype1 TEXT, area1 TEXT, numcars TEXT, numhouses REAL)');
+  db.run('CREATE TABLE IF NOT EXISTS houseCars(areacode1 TEXT, areatype1 TEXT, area1 TEXT, numcars TEXT, numhouses REAL)');
   var stmt = db.prepare('INSERT INTO houseCars (areacode1, areatype1, area1, numcars, numhouses) VALUES (?,?,?,?,?)');
   for (var i = 0; i < houseCars.length; i++) {
       stmt.run(houseCars[i].AreaCode
@@ -47,14 +47,28 @@ var houseCars = JSON.parse(fs.readFileSync('householdCars.json','utf8'))
       console.log(row.areacode1 + ": " + row.numhouses);
   });
 });
+//db.close();
+
+//output db houseCars
+var posthc = [];
+db.serialize(function() {
+    db.each("SELECT * FROM houseCars", function(err, row) {
+    posthc.push({area1: row.area1, numcars: row.numcars, numhouses: row.numhouses})//just prints out area, numcars, numhouses
+    }, function() {       
+    })
+})
+var app = express();
+app.get('/test', function(req, res) {
+    console.log("Getting houseCars data...");
+    res.send(posthc);
+});
+
 db.close();
 
-
-
-var app = express();
+/*var app = express();
 app.get('/', function(req, res) {
   res.send("This creates a table for each of the datasets.");
-});
+});*/
 
 
 
